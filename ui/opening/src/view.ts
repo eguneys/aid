5cc5339349e1a2664c41c398bc7e69cf166deda4
Emre Guneyler <eguneys@gmail.com>
2022-01-09 23:19:21 +0000

    tree view

--- ui/opening/src/view.ts
+++ ui/opening/src/view.ts
@@ -4,6 +4,12 @@ import Ctrl from './ctrl'
 import { MoveNode, MoveRoot, LightChapter, Lines, Line, isMove, isComment } from './ctrl'
 import { FRoot, FNode } from 'chesstwo'
 
+export type Opts = {
+  isMainline: boolean,
+  withIndex?: boolean
+}
+
+
 export default function view(ctrl: Ctrl) {
   return h('main.opening', [
     h('div.opening__board', board()),
@@ -47,32 +53,52 @@ export function chapters(ctrl: Ctrl) {
 }
 
 export function tview(ctrl: Ctrl) {
-  return h('div.tview', renderLines(ctrl, ctrl.line.children))
+  console.log(ctrl.line)
+  return h('div.tview', renderChildrenOf(ctrl, ctrl.line, {
+    isMainline: true
+  }))
 }
 
-
-function renderChildrenOf(ctrl: Ctrl, node: FNode<MoveNode>): Array<VNode> | undefined {
+function renderChildrenOf(ctrl: Ctrl, node: FRoot<MoveNode, MoveRoot> | FNode<MoveNode>, opts: Opts): Array<VNode> | undefined {
   let cs = node.children,
     main = cs[0]
   if (!main) return
-  return [
-    ...[renderMoveOf(ctrl, main)],
-    renderLines(ctrl, cs.slice(1)),
-    ... renderChildrenOf(ctrl, main) || []
-  ]
+  if (!cs[1]) {
+    return renderMoveAndChildrenOf(ctrl, main, { isMainline: true, withIndex: opts.withIndex })
+  }
+  if (opts.isMainline) {
+    return [
+      ...[renderMoveOf(ctrl, main, { isMainline: true, withIndex: opts.withIndex } )],
+      renderLines(ctrl, cs.slice(1), { isMainline: true }),
+      ... renderChildrenOf(ctrl, main, { isMainline: true, withIndex: true }) || []
+    ]
+  }
+
+  return [renderLines(ctrl, cs, opts)]
 }
 
-function renderLines(ctrl: Ctrl, lines: Array<FNode<MoveNode>>): VNode {
+function renderLines(ctrl: Ctrl, lines: Array<FNode<MoveNode>>, opts: Opts): VNode {
   return h('lines', lines.map(n => 
-    h('line', renderMoveAndChildrenOf(ctrl, n))
+    h('line', renderMoveAndChildrenOf(ctrl, n, { isMainline: false, withIndex: true }))
   ))
 }
 
-function renderMoveAndChildrenOf(ctrl: Ctrl, node: FNode<MoveNode>): Array<VNode> {
-  return [renderMoveOf(ctrl, node)]
-  .concat(renderChildrenOf(ctrl, node) || [])
+function renderMoveAndChildrenOf(ctrl: Ctrl, node: FNode<MoveNode>, opts: Opts): Array<VNode> {
+  return [renderMoveOf(ctrl, node, opts)]
+  .concat(renderChildrenOf(ctrl, node, { isMainline: opts.isMainline }) || [])
 }
 
-function renderMoveOf(ctrl: Ctrl, node: FNode<MoveNode>) {
-  return h('move', node.data.san)
+function renderMoveOf(ctrl: Ctrl, node: FNode<MoveNode>, opts: Opts) {
+
+  let content = [
+    opts.withIndex || node.data.ply & 1 ? renderIndex(node.data.ply) : null,
+    node.data.san
+  ]
+
+  return h('move', content)
+}
+
+function renderIndex(ply: Ply) {
+  let index = Math.ceil(ply / 2) + (ply % 2 === 0 ? '...': '.')
+  return h('index', index)
 }
